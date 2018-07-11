cedb4a7a581349234c1506d887a0b3283aed9882
Alyssa Vongries <alyssavongries@gmail.com>
2018-07-11 00:36:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
   kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
 }
